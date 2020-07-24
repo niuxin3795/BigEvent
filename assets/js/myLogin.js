abc1f07ec433d2844037fdfd68149003111298f6
niuxin <niuxin_3795@163.com>
2020-07-24 12:51:46 +0800

    新增了密码验证功能和注册功能

--- assets/js/myLogin.js
+++ assets/js/myLogin.js
@@ -7,14 +7,56 @@ $(function () {
         $(".box-login").show();
         $(".box-reg").hide();
     });
-
+    
     var form = layui.form;
+    var layer = layui.layer;
     form.verify({
         pwd: [
             /^[\S]{6,12}$/
             , '密码必须6到12位，且不能出现空格'
-        ]
-    })
+        ],
+    
+        repwd: function(value) {
+            var pwd = $("#form-reg [name=password]").val();
+            if (pwd !== value) {
+                return "两次密码不一致!";
+            };
+        }
+    
+    });
 
+    // 注册提交事件
+    $("#form-reg").on("submit", function (e) {
+        e.preventDefault();
+        var data = {
+            username: $("#form-reg [name=username]").val(),
+            password: $("#form-reg [name=password]").val()
+        };
+        $.post("http://ajax.frontend.itheima.net/api/reguser", data,
+            function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg("注册成功,请登录!");
+                $("#link_reg").click();
+            }
+        );
+    }); 
 
+    // 登录事件
+    $("#form-login").on("submit", function (e) {
+        e.preventDefault();
+        var data = {
+            username: $("#form-login [name=username]").val(),
+            password: $("#form-login [name=password]").val()
+        };
+        $.post("http://ajax.frontend.itheima.net/api/login", data,
+            function (res) {
+                if (res.status !== 0) {
+                     
+                }
+            },
+            
+        );
+    });
 })
